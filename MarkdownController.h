dcbf340142ab025613f91155d98411affe797843
Steven Wilkin <stevenwilkin@gmail.com>
2011-05-23 13:29:50 +0100

    add controler with a preview action and hook it up to the preview button

---
+++ MarkdownController.h
@@ -0,0 +1,17 @@
+//
+//  MarkdownController.h
+//  Markdown
+//
+//  Created by Steven Wilkin on 23/05/2011.
+//  Copyright 2011 NullTheory Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MarkdownController : NSObject {
+}
+
+- (IBAction)preview:(id)sender;
+
+@end
---
+++ MarkdownController.m
@@ -0,0 +1,18 @@
+//
+//  MarkdownController.m
+//  Markdown
+//
+//  Created by Steven Wilkin on 23/05/2011.
+//  Copyright 2011 NullTheory Ltd. All rights reserved.
+//
+
+#import "MarkdownController.h"
+
+
+@implementation MarkdownController
+
+- (IBAction)preview:(id)sender {
+	NSLog(@"preview");
+}
+
+@end
